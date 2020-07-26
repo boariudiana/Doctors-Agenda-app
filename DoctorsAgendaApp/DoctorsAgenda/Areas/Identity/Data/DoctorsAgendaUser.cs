7526c03d7d8e8a1b0a310ea4198509e1911e5f1c
Boariu Diana <59936212+boariudiana@users.noreply.github.com>
2020-07-26 13:24:21 +0300

    user class modified

--- DoctorsAgendaApp/DoctorsAgenda/Areas/Identity/Data/DoctorsAgendaUser.cs
+++ DoctorsAgendaApp/DoctorsAgenda/Areas/Identity/Data/DoctorsAgendaUser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +11,14 @@ namespace DoctorsAgenda.Areas.Identity.Data
     // Add profile data for application users by adding properties to the DoctorsAgendaUser class
     public class DoctorsAgendaUser : IdentityUser
     {
+        [Required]
+        [NotNull]
+        public override string Email { get => base.Email; set => base.Email = value; }
+        [Required]
+        [NotNull]
+        public override string PhoneNumber { get => base.PhoneNumber; set => base.PhoneNumber = value; }
+        [Required]
+        [NotNull]
+        public override string UserName { get => base.UserName; set => base.UserName = value; }
     }
 }
